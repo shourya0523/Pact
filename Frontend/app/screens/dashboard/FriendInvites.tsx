33368aa4989d4ef96fbccdf4e832f638cd1b781c
matte-realize <ffenyushi@gmail.com>
2025-10-09 12:19:11 -0400

    Dashboard files
    Added all dashboard files, completed Challenges tab without images

---
+++ Frontend/app/screens/dashboard/FriendInvites.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import { Image, View, Text } from 'react-native'
+
---
+++ Frontend/app/screens/dashboard/ProfileView.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, Image, TextInput } from 'react-native'
+
+export default function ProfileView() {
+    return (
+        <View>
+            <Text className="text-white text-[28px] m-4">PROFILE</Text>
+            <Image />
+            <TextInput />
+            <TextInput />
+            <TextInput />
+        </View>
+    )
+}
