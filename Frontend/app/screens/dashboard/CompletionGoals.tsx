8f0f85e658958bc816a7a37cd27b0adda8fa7c62
matte-realize <ffenyushi@gmail.com>
2025-10-29 01:27:53 -0400

    Goal pages
    Need to adjust Nativewind CSS to match ideal output

---
+++ Frontend/app/screens/dashboard/CompletionGoals.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { View, Text, TextInput, Pressable } from 'react-native'
+import { useRouter } from 'expo-router'
+import Particles from '@/components/ui/starsParticlesBackground'
+import GreyButton from '@/components/ui/greyButton';
+
+export default function Goals() {
+    const router = useRouter()
+
+    return (
+        <View className="flex-1 relative">
+            <Particles />
+            <View className="flex-1 justify-center items-center">
+                <Text className="font-wix text-white text-[38px] text-center">Create Completion Goal</Text>
+            <TextInput
+                className="w-[80%] h-[50px] bg-white/85 rounded-[15px] text-[16px] font-wix mt-16"
+                placeholder="Goal name"
+                placeholderTextColor="#3F414E"
+                style={{ paddingHorizontal: 20 }}
+            />
+            <Text className="font-wix text-white text-[24px] text-center mt-10">Description</Text>
+                <TextInput
+                    className="w-[80%] h-[120px] bg-white/85 rounded-[20px] text-[16px] font-wix mt-4"
+                    placeholder="Habit descripton"
+                    placeholderTextColor="#3F414E"
+                    multiline
+                    textAlignVertical="top"
+                    style={{ padding: 20 }}
+                />
+            
+                <Text className="font-wix text-white text-[24px] text-center mt-8">Specify Completion Value</Text>
+                <TextInput
+                    className="w-[80%] h-[50px] bg-white/85 rounded-[15px] text-[16px] font-wix mt-8"
+                    placeholder="Goal name"
+                    placeholderTextColor="#3F414E"
+                    style={{ paddingHorizontal: 20 }}
+                />
+                <View className="flex-row justify-center mt-20">
+                    <GreyButton
+                        onPress={() => router.push('/screens/dashboard/habitCreated')}
+                        text="CREATE"
+                        style={{ marginRight: 10, width: '190px', height: '65px' }}
+                    />
+                    <GreyButton
+                        onPress={() => router.push('/screens/dashboard/habitCreated')}
+                        text="SAVE"
+                        style={{ width: '190px', height: '65px'}}
+                    />
+                </View>
+            </View>
+        </View>
+    )
+}
