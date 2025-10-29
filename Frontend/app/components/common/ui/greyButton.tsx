fdc3ecf4be9b0770a7b0081f93ccc85f3820abc9
matte-realize <ffenyushi@gmail.com>
2025-10-29 01:06:53 -0400

    StudyHabit page
    Need to adjust Nativewind CSS to match ideal output, will change some of the buttons to be a UI component for easier reusage

--- Frontend/app/components/common/ui/greyButton.tsx
+++ Frontend/app/components/common/ui/greyButton.tsx
@@ -4,17 +4,23 @@ import { Pressable, Text } from 'react-native'
 interface greyButtonProps {
     onPress: () => void;
     text: string;
+    style?: object;
 }
 
 const greyButton: React.FC<greyButtonProps> = ({
     onPress,
-    text
+    text,
+    style
 }) => {
     return (
         <Pressable
             onPress={onPress}
-            className="w-[480px] h-[60px] rounded-[30px] flex-row items-center justify-center"
-            style={{ backgroundColor: "rgba(129, 132, 152, 0.27)" }} 
+            className="rounded-[30px] flex-row items-center justify-center"
+            style={[{ 
+                backgroundColor: "rgba(129, 132, 152, 0.27)", 
+                width: '480px',
+                height: '60px'
+            }, style]} 
         >
             <Text className="text-[16px] font-wix text-white text-center">
                 {text}
